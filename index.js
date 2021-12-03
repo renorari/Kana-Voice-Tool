dd90487fc24cb570dbadcc73a8f527d16b4275c4
TarochanChannel <gakutyn.2020@gmail.com>
2021-12-04 03:40:40 +0900

    見た目だけOK

---
+++ index.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow, screen } = require("electron");
+const path = require("path");
+
+const createWindow = () => {
+    const mainWindow = new BrowserWindow({
+        "title": "Kana Voice",
+        "width": screen.getPrimaryDisplay().size.width / 1.2,
+        "height": screen.getPrimaryDisplay().size.height / 1.2,
+        "icon": "",
+        "backgroundColor": "#121212",
+        "autoHideMenuBar": true,
+        "webPreferences": {
+            "preload": path.join(__dirname, "preload.js")
+        }
+    });
+
+    mainWindow.setMenu(null);
+    mainWindow.loadFile("index.html");
+
+    //mainWindow.webContents.openDevTools();
+};
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") app.quit();
+});

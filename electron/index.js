8ac8b715dce1ead78b8facc8a5e360c7b3e7f924
TarochanChannel <gakutyn.2020@gmail.com>
2021-12-03 12:06:59 +0900

    セット

---
+++ electron/index.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow, screen } = require("electron");
+const path = require("path");
+
+const createWindow = () => {
+    const mainWindow = new BrowserWindow({
+        width: screen.getPrimaryDisplay().size.width/1.2,
+        height: screen.getPrimaryDisplay().size.height/1.2,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+        },
+    });
+
+    mainWindow.loadURL("");
+
+    //mainWindow.webContents.openDevTools()
+};
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") app.quit();
+});

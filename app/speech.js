958f91967e4aa15842d62e7a1c7cfc8695000469
TarochanChannel <gakutyn.2020@gmail.com>
2022-03-19 11:04:53 +0900

    ショートカットキーの追加

--- app/speech.js
+++ app/speech.js
@@ -1,5 +1,7 @@
 var voice = new Audio();
 
+document.getElementById("input").value = localStorage.getItem("text");
+
 if (localStorage.getItem("login")) {
     document.getElementById("id").value = JSON.parse(localStorage.getItem("login")).id;
     document.getElementById("password").value = JSON.parse(localStorage.getItem("login")).password;
@@ -16,10 +18,12 @@ fetch("https://kana.renorari.net/api/voice_list.json", { "method": "GET" }).then
 });
 
 document.getElementById("play_btn").onclick = async () => {
-    fetch(`https://kana.renorari.net/api/voice`, { "method": "POST", "body": `text=${encodeURI(document.getElementById("input").value)}&id=${encodeURI(document.getElementById("id").value)}&password=${encodeURI(document.getElementById("password").value)}&voice=${document.getElementById("voice").value}` }).then((res) => {
+    document.getElementById("input").setAttribute("disabled", null);
+    fetch(`https://kana.renorari.net/api/voice`, { "method": "POST", "body": `text=${encodeURI(document.getElementById("input").value.replace(/\n/g, " "))}&id=${encodeURI(document.getElementById("id").value)}&password=${encodeURI(document.getElementById("password").value)}&voice=${document.getElementById("voice").value}` }).then((res) => {
         if (res.status == 200) return [res.url];
         else return res.text();
     }).then((data) => {
+        document.getElementById("input").removeAttribute("disabled");
         if (typeof data == "string") return alert(`エラーが発生しました。\n\nエラー: ${data}`);
         localStorage.setItem("login", JSON.stringify({ "id": document.getElementById("id").value, "password": document.getElementById("password").value }));
         localStorage.setItem("voice", document.getElementById("voice").value);
@@ -32,7 +36,10 @@ document.getElementById("play_btn").onclick = async () => {
     });
 };
 
-document.getElementById("stop_btn").onclick = () => voice.pause();
+document.getElementById("stop_btn").onclick = () => {
+    voice.pause();
+    document.getElementById("input").removeAttribute("disabled");
+};
 
 document.getElementById("download_btn").onclick = () => {
     fetch(`https://kana.renorari.net/api/voice`, { "method": "POST", "body": `text=${encodeURI(document.getElementById("input").value)}&id=${encodeURI(document.getElementById("id").value)}&password=${encodeURI(document.getElementById("password").value)}&voice=${document.getElementById("voice").value}` }).then((res) => {
@@ -55,3 +62,20 @@ document.getElementById("download_btn").onclick = () => {
         alert(`エラーが発生しました。\n\nエラー: ${error}`);
     });
 };
+
+document.onkeydown = (event) => {
+    if (event.key == ("F5" || " ")) {
+        document.getElementById("play_btn").onclick();
+        return false;
+    };
+    if (event.key == ("Delete")) {
+        document.getElementById("stop_btn").onclick();
+        return false;
+    };
+    if (event.key == "F6") {
+        document.getElementById("download_btn").onclick();
+        return false;
+    };
+
+    localStorage.setItem("text", document.getElementById("input").value);
+}
